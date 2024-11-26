763d5526354b3d9136d9f72f8cee37afdfd3b24c
Lucas Vestavik Haugen <94321875+Fainxc@users.noreply.github.com>
2024-11-26 23:32:42 +0100

    Delete PracticeBotDevv directory

--- PracticeBotDevv/Datastores/BlacklistData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const Schema = new mongoose.Schema({
-    GuildId: { type: Number },
-    UserId: { type: Number },
-    Reason: { type: String },
-});
-
-module.exports = mongoose.model("BlacklistData", Schema)
--- PracticeBotDevv/Datastores/BotblacklistData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const Schema = new mongoose.Schema({
-    UserId: { type: Number },
-    Reason: { type: String },
-});
-
-module.exports = mongoose.model("BotBlacklistsData", Schema)
